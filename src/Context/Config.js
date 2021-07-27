6253ce730caab28483273b8058f333e31edcc8e9
coltoneshaw <coltoneshaw@gmail.com>
2021-07-27 11:38:30 -0400

    Moved test defaultConfig onto the config context. Will need to remove this and pull from config.js:

--- src/Context/Config.js
+++ src/Context/Config.js
@@ -5,7 +5,6 @@ import { sub, getTime } from 'date-fns'
 // const GlobalDispatchContext = React.createContext();
 const ConfigContext = createContext();
 
-import { defaultConfig } from '../utils/defaultConfig';
 
 // finding the data that exists based on the dotprop.
 const findData = (config, path) => {
@@ -13,6 +12,32 @@ const findData = (config, path) => {
     console.log({ config, path })
     if (dotProp.has(config, path)) return dotProp.get(config, path)
     return ""
+}
+
+
+const defaultConfig = {
+    "apis": {
+        "threeC": {
+            "key": "21b6a7f20eb0448e8c642aa1a98180ca5ca6d27477484139aea5881d7ba5c034",
+            "secret": "1edbe2e48e11674e0fcc53d64ae540d8fbb81314b3d3ae155d45eec48069d3be731183119eb7c54ebaedc58e038aac394de6f975cdcd74a2215fe9830c8a53fc9816679599f6efbfd23a0435549bfb41334a5c1bebb71db3d2d701accaa94360c0b33008"
+        }
+    },
+    "general": {
+        "defaultCurrency": "USD",
+        "globalLimit": 250000
+    },
+    "syncStatus": {
+        "deals": {
+            "lastSyncTime": 1626958843865
+        }
+    },
+    "database": {
+        "type": "local"
+    },
+    "statSettings": {
+        "startDate": getTime(sub(new Date(), { days: 90 })),
+        "account_id": ""
+    }
 }
 
 
