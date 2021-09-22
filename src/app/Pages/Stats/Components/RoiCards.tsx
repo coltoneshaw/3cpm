d4ad6e82b9b04ea5cc7ad327ca77af42a1f72414
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2021-09-22 08:26:18 -0400

    Adjusted metric cards on stats to be ordered the same across pages

---
+++ src/app/Pages/Stats/Components/RoiCards.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import {
+    Card_ActiveDeals, Card_totalInDeals, Card_MaxDca,
+    Card_TotalBankRoll, Card_TotalProfit, Card_MaxRiskPercent,
+    Card_TotalBoughtVolume, Card_TotalDeals, Card_TotalRoi,
+    Card_AverageDailyProfit, Card_AverageDealHours
+} from '@/app/Components/Charts/DataCards';
+
+import { Type_MetricData } from "@/types/3Commas";
+
+const RoiCards = ({ metricsData, currentView }: { currentView: string, metricsData: Type_MetricData }) => {
+
+    const {
+        activeDealCount, totalInDeals, maxRisk, totalBankroll, position, on_orders,
+        totalProfit, totalBoughtVolume, reservedFundsTotal, maxRiskPercent, totalDeals,
+        boughtVolume, totalProfit_perf, averageDailyProfit, averageDealHours, totalClosedDeals, totalDealHours } = metricsData
+
+    const additionalMetrics = (currentView: string) => {
+        if (currentView === 'performance-monitor') {
+            return (
+                <>
+                    <Card_TotalProfit metric={totalProfit} />
+                    <Card_TotalDeals metric={totalDeals} />
+                    <Card_AverageDailyProfit metric={averageDailyProfit} />
+                    <Card_AverageDealHours metric={averageDealHours} additionalData={{ totalClosedDeals, totalDealHours }} />
+                </>)
+        } else if (currentView === 'risk-monitor') {
+            return (
+                <>
+                    <Card_MaxRiskPercent metric={maxRiskPercent} additionalData={{ totalBankroll, maxDCA: maxRisk }} />
+                    <Card_ActiveDeals metric={activeDealCount} />
+                    <Card_totalInDeals metric={totalInDeals} additionalData={{ on_orders, totalBoughtVolume }} />
+                    <Card_TotalBankRoll metric={totalBankroll} additionalData={{ position, totalBoughtVolume, reservedFundsTotal }} />
+                </>)
+        }
+
+        return (
+            <>
+                <Card_TotalProfit metric={totalProfit} />
+                <Card_ActiveDeals metric={activeDealCount} />
+                <Card_totalInDeals metric={totalInDeals} additionalData={{ on_orders, totalBoughtVolume }} />
+                <Card_TotalBankRoll metric={totalBankroll} additionalData={{ position, totalBoughtVolume, reservedFundsTotal }} />
+            </>)
+    }
+
+
+    return (
+        <div className="flex-column" style={{ alignItems: 'center' }}>
+            <div className="riskDiv" style={{ paddingBottom: '32px' }}>
+                <Card_MaxDca metric={maxRisk} />
+                <Card_TotalRoi additionalData={{ totalProfit, totalBankroll }} />
+                {additionalMetrics(currentView)}
+            </div>
+        </div>
+    )
+}
+
+export default RoiCards
