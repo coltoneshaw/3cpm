88c8b3505aa6894f2fd26eade7098b5fb6da3be2
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2021-10-03 14:37:32 -0400

    rearranged the state, cleaned up some ifs.

--- src/app/Components/Buttons/ToggleRefresh.tsx
+++ src/app/Components/Buttons/ToggleRefresh.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Button, LinearProgress } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
@@ -12,9 +12,9 @@ interface Type_ButtonProps {
 }
 
 import './ToggleRefresh.scss'
-import {useAppDispatch, useAppSelector} from "@/app/redux/hooks";
-import {setAutoRefresh} from "@/app/redux/threeCommas/threeCommasSlice";
-import {refreshFunction} from "@/app/redux/threeCommas/Actions";
+import { useAppDispatch, useAppSelector } from "@/app/redux/hooks";
+import { setAutoRefresh } from "@/app/redux/threeCommas/threeCommasSlice";
+import { refreshFunction } from "@/app/redux/threeCommas/Actions";
 
 
 /**
@@ -23,24 +23,24 @@ import {refreshFunction} from "@/app/redux/threeCommas/Actions";
  * - Move the state of this timer somewhere shared so it doesn't continue to cause issues with updating
  */
 const ToggleRefreshButton = ({ style, className }: Type_ButtonProps) => {
+    const { autoRefresh, isSyncingTime, isSyncing, syncOptions } = useAppSelector(state => state.threeCommas);
+    const dispatch = useAppDispatch();
+
     const refreshRate = 500;
     const max = 15000;
 
-    const { autoRefresh, isSyncingTime, isSyncing, syncOptions } = useAppSelector(state => state.threeCommas);
+    const [timeout, setActualTimeout] = useState<NodeJS.Timeout | null>(null);
     const [counter, setCounter] = useState<number>(() => {
         if (autoRefresh) {
-            if (isSyncingTime > 0) return isSyncingTime+max-Date.now()
-            if (syncOptions.time && syncOptions.time > 0) return syncOptions.time+max-Date.now()
+            if (isSyncingTime > 0) return isSyncingTime + max - Date.now()
+            if (syncOptions.time && syncOptions.time > 0) return syncOptions.time + max - Date.now()
         }
         return 0
     });
-    const dispatch = useAppDispatch();
 
 
     useEffect(() => {
-        if (autoRefresh && counter <= 0) {
-            setCounter(max)
-        }
+        if (autoRefresh && counter <= 0) setCounter(max)
     }, [autoRefresh]);
 
     useEffect(() => {
@@ -51,17 +51,13 @@ const ToggleRefreshButton = ({ style, className }: Type_ButtonProps) => {
     }, [isSyncing])
 
 
-    const [timeout, setActualTimeout] = useState<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
-        if (!autoRefresh) return
-        if (isSyncing) return
+        if (!autoRefresh || isSyncing) return
 
         if (counter > 0) {
             setActualTimeout(setTimeout(() => {
-                setCounter(prevState => {
-                    return Math.max(prevState - refreshRate, 0);
-                })
+                setCounter(prevState =>  Math.max(prevState - refreshRate, 0))
             }, refreshRate));
         }
     }, [counter, isSyncing]);
@@ -83,8 +79,6 @@ const ToggleRefreshButton = ({ style, className }: Type_ButtonProps) => {
         refreshFunction('run', 200)
     }
 
-    const percent = 100-(counter*100/max)
-    
 
     return (
         <Button
@@ -93,14 +87,14 @@ const ToggleRefreshButton = ({ style, className }: Type_ButtonProps) => {
             className={className}
             onClick={onClick}
             disableElevation
-            startIcon={ (autoRefresh) ? <StopIcon /> : <PlayArrowIcon/> }
+            startIcon={(autoRefresh) ? <StopIcon /> : <PlayArrowIcon />}
             style={{
-                ...style, 
+                ...style,
                 backgroundColor: 'var(--color-primary)',
                 color: 'var(--color-text-lightbackground)'
             }}
         > Auto Refresh
-            {autoRefresh && (<LinearProgress variant="determinate" value={percent} />)}
+            {autoRefresh && (<LinearProgress variant="determinate" value={100 - (counter * 100 / max)} />)}
         </Button>
     )
 }
