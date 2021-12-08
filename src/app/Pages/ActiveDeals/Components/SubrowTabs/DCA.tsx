f4a26f508e5cd1fb692151cceccc0796bf315ab4
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2021-12-08 14:10:15 -0500

    Adjusted DCA table calcs, added total profit, cleaned up the code

--- src/app/Pages/ActiveDeals/Components/SubrowTabs/DCA.tsx
+++ src/app/Pages/ActiveDeals/Components/SubrowTabs/DCA.tsx
@@ -1,6 +1,7 @@
 import { Grid, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { parseNumber } from '@/utils/number_formatting'
+import { Type_ActiveDeals, Type_MarketOrders } from "@/types/3Commas";
 
 
 type calc = {
@@ -9,92 +10,139 @@ type calc = {
     tpPercent: number
 }
 
-const exchangeFee = 0.001
-
-const calcNew = ({addFunds, atPrice, tpPercent}: calc, ordersData: any, currentPrice: string) => {
-    const filteredData = ordersData.filter((r: any) => r.order_type === 'BUY' && r.status_string === 'Filled')
-    const totalCrypto = filteredData.reduce((r: number, c: any) => {
-        return r + parseFloat(c.quantity)
-    }, 0) + addFunds / atPrice
-
-
-    const totalSpent = filteredData.reduce((r: number, c: any) => {
-        return r + parseFloat(c.total)
-    }, 0) + addFunds
-
-    const average = totalSpent / totalCrypto
-    const tpAt = ( average * ( ( tpPercent / 100) + exchangeFee ) ) + average
-
-    return {
-        average,
-        tpAt,
-        gainRequired: ( ( ( tpAt / average ) - 1) + ((average / +currentPrice) -1) ) * 100
-    }
+type CalcObject = {
+    average: number,
+    tpAt: number,
+    tpPercent: number,
+    gainRequired: number,
+    name: string,
+    totalProfit: number
 }
-
 
 type row = {
-    bought_average_price: number,
-    take_profit: number
-    current_price: number
+    original: Type_ActiveDeals
 }
 
-const calcOriginal = ({ bought_average_price, take_profit, current_price}: row ) => {
-    const average = bought_average_price;
-    const tpPercent = take_profit / 100;
-    const tpAt = average * ( 1 + tpPercent + exchangeFee);
+type DCA = {
+    row: row,
+    ordersData: Type_MarketOrders[] | []
+}
+
+const exchangeFee = 0.001
+
+const calcNew = ({ addFunds, atPrice, tpPercent }: calc, ordersData: Type_MarketOrders[], currentPrice: number, bought_volume: number) => {
+    const filteredData = ordersData.filter(r => r.order_type === 'BUY' && r.status_string === 'Filled')
+    const totalCrypto = filteredData.reduce((r, c) => r + c.quantity, 0) + addFunds / atPrice
+    const totalSpent = filteredData.reduce((r, c) => r + c.total, 0) + addFunds
+    const average = totalSpent / totalCrypto
+
+    const tpAt = average * (1 + (tpPercent/100) + exchangeFee);
     return {
-        average, 
+        name: 'New',
+        average,
         tpAt,
-        gainRequired: ( ( ( tpAt / average ) - 1) + ((average / current_price) -1) ) * 100
+        gainRequired: (((tpAt / average) - 1) + ((average / +currentPrice) - 1)) * 100,
+        tpPercent,
+        totalProfit: ( tpPercent / 100 ) * ( bought_volume + addFunds )
     }
-
 }
 
-function DCA({ row, ordersData }: any) {
+const calcOriginal = ({ bought_average_price, take_profit, current_price, bought_volume }: Type_ActiveDeals) => {
+    const tpPercent = take_profit / 100;
+    const tpAt = bought_average_price * (1 + tpPercent + exchangeFee);
+
+    return {
+        name: 'Original',
+        average: bought_average_price,
+        tpAt,
+        gainRequired: (((tpAt / bought_average_price) - 1) + ((bought_average_price / current_price) - 1)) * 100,
+        tpPercent: take_profit,
+        totalProfit: ( take_profit / 100 ) * bought_volume
+    }
+}
 
 
-    const origCalc = calcOriginal(row.original)
-    const [newCalc, setNewCalc] = useState(() => ({ average: origCalc.average, tpAt: origCalc.tpAt, gainRequired: origCalc.gainRequired }))
+const dcaTableData = (row: row, calc: calc, ordersData: Type_MarketOrders[]): CalcObject[] => {
+    const original = calcOriginal(row.original)
+    const newData = calcNew(calc, ordersData, row.original.current_price, row.original.bought_volume)
+    const diff = {
+        name: "Difference",
+        average: newData.average - original.average,
+        tpAt: newData.tpAt - original.tpAt,
+        tpPercent: newData.tpPercent - row.original.take_profit,
+        gainRequired:  newData.gainRequired - original.gainRequired,
+        totalProfit: newData.totalProfit - original.totalProfit
+    }
+    return [original, newData, diff]
+}
 
-    const [addFunds, setAddFundsField] = useState<number>(0);
-    const [atPrice, setAtPrice] = useState<number>(parseFloat(row.original.current_price));
-    const [tpPercent, setTpPercent] = useState<number>(parseFloat(row.original.take_profit));
+
+
+function DCA({ row, ordersData }: DCA) {
+
+    const [addFunds, setAddFundsField] = useState(0);
+    const [atPrice, setAtPrice] = useState(row.original.current_price);
+    const [tpPercent, setTpPercent] = useState(row.original.take_profit);
+
+    const calcObject = dcaTableData(row, { addFunds, atPrice, tpPercent }, ordersData)
 
     useEffect(() => {
         if (isNaN(addFunds)) {
             setAddFundsField(0)
             return
         }
-
-        // It's receiving the price in atPrice and using that as the TP price, I believe
-        setNewCalc(calcNew({ addFunds, atPrice, tpPercent }, ordersData, row.original.current_price))
-
     }, [addFunds, atPrice, tpPercent])
 
     return (
         <div className="flex-column" style={{ width: '100%' }}>
             <Grid container spacing={3}>
                 <Grid item xs={4}>
-                    <TextField label={"Add funds (" + row.original.from_currency + ")"} value={addFunds} onChange={event => {
-                        setAddFundsField(parseFloat(event.target.value))
-                    }} type="number" fullWidth />
+                    <TextField
+                        label={"Add funds (" + row.original.from_currency + ")"}
+                        value={addFunds}
+                        onChange={event => {
+                            setAddFundsField(parseFloat(event.target.value))
+                        }}
+                        type="number"
+                        fullWidth
+                    />
 
                 </Grid>
                 <Grid item xs={4}>
-                    <TextField label={"At Price (" + row.original.to_currency + ")"} value={atPrice} onChange={event => {
-                        setAtPrice(parseFloat(event.target.value))
-                    }} type="number" fullWidth />
+                    <TextField
+                        label={"At Price (" + row.original.to_currency + ")"}
+                        value={atPrice}
+                        onChange={event => {
+                            setAtPrice(parseFloat(event.target.value))
+                        }}
+                        type="number"
+                        fullWidth
+                    />
                 </Grid>
                 <Grid item xs={4}>
-                    <TextField label="Take Profit (%)" value={tpPercent} onChange={event => {
-                        setTpPercent(parseFloat(event.target.value))
-                    }} type="number" fullWidth />
+                    <TextField
+                        label="Take Profit (%)"
+                        value={tpPercent}
+                        onChange={event => {
+                            setTpPercent(parseFloat(event.target.value))
+                        }}
+                        type="number"
+                        fullWidth
+                    />
                 </Grid>
             </Grid>
 
 
-            <table style={{ tableLayout: 'fixed', margin: 'auto', marginTop: "2rem", width: '70%', alignSelf: 'center' }} className="dcaTable">
+            <table 
+                style={{ 
+                    tableLayout: 'fixed', 
+                    margin: 'auto', 
+                    marginTop: "2rem", 
+                    width: '70%', 
+                    alignSelf: 'center'
+                }} 
+                className="table table-bordered table-striped RUBYDEV__deals_table_thead_border_fix"
+            >
                 <thead>
                     <tr>
                         <th></th>
@@ -102,30 +150,22 @@ function DCA({ row, ordersData }: any) {
                         <th>Take profit at</th>
                         <th>Take profit percent</th>
                         <th>Distance from TP</th>
+                        <th>Total Profit</th>
                     </tr>
                 </thead>
                 <tbody className="dcaCalcTable">
-                    <tr>
-                        <td>Original</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.average, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.tpAt, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(row.original.take_profit, 2)}%</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.gainRequired, 2)}%</td>
-                    </tr>
-                    <tr>
-                        <td>New</td>
-                        <td className=" monospace-cell">{parseNumber(newCalc.average, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(newCalc.tpAt, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(tpPercent, 2)}%</td>
-                        <td className=" monospace-cell">{parseNumber(newCalc.gainRequired, 2)}%</td>
-                    </tr>
-                    <tr className="summaryRow">
-                        <td>Difference</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.average - newCalc.average, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.tpAt - newCalc.tpAt, 6)}</td>
-                        <td className=" monospace-cell">{parseNumber(row.original.take_profit - tpPercent, 2)}%</td>
-                        <td className=" monospace-cell">{parseNumber(origCalc.gainRequired - newCalc.gainRequired, 2)}%</td>
-                    </tr>
+                    {calcObject.map(row => {
+                        return (
+                            <tr key={row.name}>
+                                <td>{row.name}</td>
+                                <td className=" monospace-cell">{parseNumber(row.average, 6)}</td>
+                                <td className=" monospace-cell">{parseNumber(row.tpAt, 6)}</td>
+                                <td className=" monospace-cell">{parseNumber(row.tpPercent, 2)}%</td>
+                                <td className=" monospace-cell">{parseNumber(row.gainRequired, 2)}%</td>
+                                <td className=" monospace-cell">{parseNumber(row.totalProfit, 2)}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>)
