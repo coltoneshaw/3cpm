94236449bcd42e6215ab630672da53687b777ff0
Colton Shaw <46071821+coltoneshaw@users.noreply.github.com>
2022-04-05 14:05:07 -0400

    Removed babel

--- webpack.webapp.js
+++ webpack.webapp.js
@@ -10,23 +10,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [[
-              '@babel/preset-env', {
-                targets: {
-                  esmodules: true,
-                },
-              }],
-              '@babel/preset-react',
-            ],
-          },
-        },
-      },
-      {
         test: /\.s[ac]ss$/i,
         use: [
           'style-loader',
